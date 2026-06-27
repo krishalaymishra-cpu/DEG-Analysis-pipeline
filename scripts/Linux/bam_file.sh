c710319945b73249068856cbf642c956042e9873
Krishalay <krishalaymishra@gmail.com>
2026-06-27 11:16:00 +0530

    Update bam_file.sh

--- scripts/Linux/bam_file.sh
+++ scripts/Linux/bam_file.sh
@@ -1,23 +1,32 @@
+### Step 1: create genome index using HISAT2 indexing tool using reference genome. the reference genome i used was Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa
+hisat2-build reference.fa genome_index
+
+### Step 2: align for bam file generation using parallel processing
 #!/bin/bash
+#path to hisat2 index
+index=/path/to/hisat_index/*.ht2
+fastq_dir= path/to/trimmed_files/
+out_dir=/mnt/c/users/bionet3/onedrive/desktop/gse138109/hisat_res
+mkdir -p $out_dir
 
-### Step 2: Align reads and create sorted BAM
+#loop through all _1 files
+for R1 in $fastq_dir/*_1.trimmed.fastq.gz
+do
+        sample=$(basename $R1 _1.trimmed.fastq.gz)
+        R2=$fastq_dir/${sample}_2.trimmed.fastq.gz
+        echo "processing $sample .."
 
-# HISAT2 index prefix (must match what you used in hisat2-build)
-INDEX_PREFIX="path/to/store/genome"
+        #align hisat2 pair reads
+        hisat2 -p 16 \
+               -x $index \
+               -1 $R1 -2 $R2 \
+               -S $out_dir/${sample}.sam \
+               2> $out_dir/${sample}.log
+        #convert sam->bam
+        samtools view -@ 16 -bS $out_dir/${sample}.sam | samtools sort
+        samtools index $out_dir/${sample}.sorted.bam
 
-# Input FASTQ files
-READ1="path/to/sample_1_trimmed_new.fastq.gz"
-READ2="path/to/sample_2_trimmed_new.fastq.gz"
+        #optional cleanup
+        rm $out_dir/${sample}.sam
 
-# Output BAM file
-SORTED_BAM= $outputdir
-
-# Run HISAT2 and pipe directly to samtools
-hisat2 -x $INDEX_PREFIX -1 $READ1 -2 $READ2 \
-  | samtools view -b \
-  | samtools sort -o $SORTED_BAM
-
-# Index BAM file
-samtools index $SORTED_BAM
-
-echo "Alignment complete. Sorted BAM file: $SORTED_BAM"
+done
